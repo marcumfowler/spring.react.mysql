3a8b1bbe49869eed921da8841ff6ba8aed60c6c6
Marc Fowler <marcum.fowler@gmail.com>
2023-02-05 18:45:16 -0500

    adds user.java

---
+++ src/main/java/com/magmutual/spring/react/mysql/modal/User.java
@@ -0,0 +1,85 @@
+package com.magmutual.spring.react.mysql.modal;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "userInformation")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Integer id;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private String email;
+    @Column
+    private String profession;
+    @Column
+    private String country;
+    @Column
+    private String city;
+    @Column
+    private LocalDate date;
+    @Override
+    public String toString() {
+        return "User [id= " + id + ", name=" + firstName + " " + lastName +"]";
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getFirstNameName() {
+        return firstName;
+    }
+    public void setFirstName(String name) {
+        this.firstName = firstName;
+    }
+    public String getLastNameName() {
+        return lastName;
+    }
+    public void setLastName(String name) {
+        this.lastName = lastName;
+    }
+    public String getProfession() {
+        return profession;
+    }
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public LocalDate getDate() {
+        return date;
+    }
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+}
