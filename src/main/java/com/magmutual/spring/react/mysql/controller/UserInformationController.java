c6d56d2ade02dffeb49842692a546f676121d91a
Marc Fowler <marcum.fowler@gmail.com>
2023-02-05 19:39:33 -0500

    adds controller

--- src/main/java/com/magmutual/spring/react/mysql/controller/UserInformationController.java
+++ src/main/java/com/magmutual/spring/react/mysql/controller/UserInformationController.java
@@ -1,4 +1,54 @@
 package com.magmutual.spring.react.mysql.controller;
 
+import java.util.List;
+
+import com.magmutual.spring.react.mysql.modal.User;
+import com.magmutual.spring.react.mysql.service.UserInformationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class UserInformationController {
+
+    @Autowired
+    private UserInformationService userInformationService;
+
+    @GetMapping("/users")
+    public List<User> get() {
+        return userInformationService.get();
+    }
+
+    @PostMapping("/users")
+    public User save(@RequestBody User user) {
+        userInformationService.save(user);
+        return user;
+    }
+
+    @GetMapping("/users/{id}")
+    public User get(@PathVariable int id) {
+        return userInformationService.get(id);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String delete(@PathVariable int id) {
+
+        userInformationService.delete(id);
+
+        return "User removed with id " + id;
+
+    }
+
+    @PutMapping("/users")
+    public User update(@RequestBody User user) {
+        userInformationService.save(user);
+        return user;
+    }
 }
