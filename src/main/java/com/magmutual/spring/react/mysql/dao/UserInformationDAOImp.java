6da31aa1a9fb1be4c02e475ff1e0c86116f7d4db
Marc Fowler <marcum.fowler@gmail.com>
2023-02-05 19:11:56 -0500

    adds data base access objects and connection

--- src/main/java/com/magmutual/spring/react/mysql/dao/UserInformationDAOImp.java
+++ src/main/java/com/magmutual/spring/react/mysql/dao/UserInformationDAOImp.java
@@ -1,4 +1,41 @@
 package com.magmutual.spring.react.mysql.dao;
 
-public class UserInformationDAOImp {
+import java.util.List;
+import jakarta.persistence.EntityManager;
+
+import com.magmutual.spring.react.mysql.modal.User;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+@Repository
+public class UserInformationDAOImp implements UserInformationDAO {
+    @Autowired
+    private EntityManager entityManager;
+    @Override
+    public List<User> get() {
+        Session currSession = entityManager.unwrap(Session.class);
+        Query<User> query = currSession.createQuery("from Employee", Employee.class);
+        List<User> list = query.getResultList();
+        return list;
+    }
+    @Override
+    public User get(int id) {
+        Session currSession = entityManager.unwrap(Session.class);
+        User emp = currSession.get(User.class, id);
+        return emp;
+    }
+    @Override
+    public void save(User user) {
+
+        Session currSession = entityManager.unwrap(Session.class);
+        currSession.saveOrUpdate(user);
+    }
+    @Override
+    public void delete(int id) {
+        Session currSession = entityManager.unwrap(Session.class);
+        User emp = currSession.get(User.class, id);
+        currSession.delete(emp);
+    }
 }
+
